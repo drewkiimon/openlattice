08743bff0713c795e5f053428448d0d7f4f076fd
drewkiimon <andrew.r.pagan@gmail.com>
2018-08-31 14:15:08 -0700

    scrollbar css

--- src/components/DetailedView.js
+++ src/components/DetailedView.js
@@ -39,6 +39,18 @@ const TableValues = styled.div`
   max-height: 100px;
   min-height: 33px;
   overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 `;
 
 class DetailedView extends Component {
