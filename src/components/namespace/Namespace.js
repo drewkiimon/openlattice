b795c963202ae21730092989ff18d0acf9341500
drewkiimon <andrew.r.pagan@gmail.com>
2018-08-31 17:20:39 -0700

    Got all namespace bins

--- src/components/namespace/Namespace.js
+++ src/components/namespace/Namespace.js
@@ -12,7 +12,78 @@ const WrapperDiv = styled.div`
   padding: 0.5em 0;
 `;
 
+const ColumnDiv = styled.div`
+  background-color: white;
+  box-sizing: border-box;
+  border: 1px solid gray;
+  border-radius: 5px;
+  -webkit-box-shadow: 6px 9px 22px -10px rgba(0, 0, 0, 0.69);
+  -moz-box-shadow: 6px 9px 22px -10px rgba(0, 0, 0, 0.69);
+  box-shadow: 6px 9px 22px -10px rgba(0, 0, 0, 0.69);
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`;
+
 class Namespace extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      propertyNamespaces: {},
+      gotPropertyNamespaces: false,
+      entityNamespaces: [],
+      gotEntityNamespaces: false
+    };
+    this.createNamespaces = this.createNamespaces.bind(this);
+  }
+
+  componentDidUpdate() {
+    const { gotProperties, gotEntities, gotAssociations } = this.props.open;
+    const { gotPropertyNamespaces, gotEntityNamespaces } = this.state;
+    if (
+      gotProperties &&
+      gotEntities &&
+      gotAssociations &&
+      !gotPropertyNamespaces & !gotEntityNamespaces
+    ) {
+      const { properties, entities } = this.props.open;
+      const propertyNamespaces = this.createNamespaces(properties);
+      const entityNamespaces = this.createNamespaces(entities);
+      this.setState({
+        propertyNamespaces,
+        gotPropertyNamespaces: true,
+        entityNamespaces,
+        gotEntityNamespaces: true
+      });
+    }
+  }
+
+  createNamespaces(properties) {
+    const namespace = {};
+    for (var i = 0; i < properties.length; i++) {
+      let item = properties[i];
+      if (!namespace[item.type.namespace]) {
+        namespace[item.type.namespace] = [item];
+      } else {
+        namespace[item.type.namespace] = [
+          ...namespace[item.type.namespace],
+          item
+        ];
+      }
+    }
+    return namespace;
+  }
+
   componentDidMount() {
     const { gotProperties, gotEntities, gotAssociations } = this.props.open;
     // If we do not have the data in state, go get it
@@ -30,6 +101,7 @@ class Namespace extends Component {
       selectedEDM
     } = this.props.open;
     const isDataReady = gotProperties && gotEntities && gotAssociations;
+
     return (
       <WrapperDiv>
         <div
@@ -40,7 +112,12 @@ class Namespace extends Component {
           }
         >
           {isDataReady ? (
-            <div className="row h-100">Woof</div>
+            <div className="row h-100">
+              <ColumnDiv className="col-5 offset-1 h-100 mr-2 p-3">
+                Hello
+              </ColumnDiv>
+              <ColumnDiv className="col-5 h-100 p-3">Goodbye</ColumnDiv>
+            </div>
           ) : (
             <Loading type={"bubbles"} color={"black"} />
           )}
